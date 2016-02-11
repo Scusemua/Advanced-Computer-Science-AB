e285fe4ec0aba7a99ed15af6b0b5b8d413235f94
BuildTools <benrcarver@gmail.com>
2016-02-10 20:09:33 -0500

    Empty Set class for Project 8.

---
+++ Advanced CS Projects/src/Project8/MySet.java
@@ -0,0 +1,64 @@
+package Project8;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+import Project7.*;
+
+public class MySet<T extends Comparable<T>> extends MyBinarySearchTree<T> implements Set<T> {
+
+	@Override
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends T> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
